70d75133c5d046f44480c6e8cb654fe7637821bf
godsfist10 <paul.kubik@mymail.champlain.edu>
2014-09-13 06:53:09 -0400

    added multiple types of throwing and adjusted gravity

--- DisgruntledEmployee/Assets/Scripts/PlayerMover.cs
+++ DisgruntledEmployee/Assets/Scripts/PlayerMover.cs
@@ -5,11 +5,13 @@ public class PlayerMover : MonoBehaviour {
 
 	public Rigidbody m_Rigidbody;
 	public float speed = .5f;
-	public float jumpHeight = 5;
 	public float hitDistance = 3;
 	public float pushForce = 10;
+	public float whipForce = 10;
+	public float maxWhipForce = 40;
 	public GameObject eyes;
-
+	protected GameObject m_holdingObject = null;
+	[SerializeField] protected bool velocityThrow = false;
 	void Start () 
 	{
 		
@@ -21,30 +23,37 @@ public class PlayerMover : MonoBehaviour {
 
 		if( Input.GetKey(KeyCode.W))
      	{
-			tempVel += transform.forward * speed;
+			tempVel += transform.forward;
 		}
 		if( Input.GetKey(KeyCode.A))
 		{
-			tempVel -= transform.right * speed;
+			tempVel -= transform.right;
 		}
 		if( Input.GetKey(KeyCode.D))
 		{
-			tempVel += transform.right * speed;
+			tempVel += transform.right;
 		}
 		if( Input.GetKey(KeyCode.S))
 		{
-			tempVel -= transform.forward * speed;
+			tempVel -= transform.forward;
 		}
 		if(Input.GetMouseButtonDown(0))
 		{
-			Ray ray = new Ray(eyes.transform.position, eyes.transform.forward);
-			RaycastHit hit;
-			int layerMask = 1 << 8;
-			if( Physics.Raycast(ray, out hit, hitDistance, layerMask))
+			if( m_holdingObject != null)
 			{
-				Debug.DrawRay( eyes.transform.position, eyes.transform.forward * hitDistance, Color.white);
-				hit.rigidbody.AddForceAtPosition(eyes.transform.forward * pushForce ,hit.point,ForceMode.Impulse);
-				Debug.Log("hit left click");
+				preThrowDrop();
+				throwForward();
+			}
+			else
+			{
+				Ray ray = new Ray(eyes.transform.position, eyes.transform.forward);
+				RaycastHit hit;
+				int layerMask = 1 << 8;
+				if( Physics.Raycast(ray, out hit, hitDistance, layerMask))
+				{
+					Debug.DrawRay( eyes.transform.position, eyes.transform.forward * hitDistance, Color.white);
+					hit.rigidbody.AddForceAtPosition(eyes.transform.forward * pushForce ,hit.point,ForceMode.Impulse);
+				}
 			}
 		}
 		if(Input.GetMouseButtonDown(1))
@@ -54,11 +63,65 @@ public class PlayerMover : MonoBehaviour {
 			int layerMask = 1 << 8;
 			if( Physics.Raycast(ray, out hit, hitDistance, layerMask))
 			{
-				hit.rigidbody.AddForceAtPosition(eyes.transform.forward * -pushForce ,hit.point,ForceMode.Impulse);
-				Debug.Log("hit right click");
+				if( hit.transform.tag == "Throwable")
+				{
+					m_holdingObject = hit.transform.gameObject;
+					m_holdingObject.rigidbody.isKinematic = true;
+					m_holdingObject.rigidbody.detectCollisions = false;
+					m_holdingObject.transform.parent = eyes.transform;
+					//Debug.Log("grabbed");
+				}
+			}
+		}
+		else if( Input.GetMouseButtonUp(1))
+		{
+			if( m_holdingObject != null)
+			{
+				preThrowDrop();
+				if(velocityThrow)
+					whipObject();
+				else
+					lightlyDrop();
+
 			}
 		}
 
-		rigidbody.velocity = tempVel * Time.deltaTime;
+		rigidbody.velocity = tempVel.normalized * speed * Time.deltaTime; 
 	}
+
+	void preThrowDrop()
+	{
+		m_holdingObject.transform.parent = null;
+		m_holdingObject.rigidbody.isKinematic = false;
+		m_holdingObject.rigidbody.detectCollisions = true;
+		
+	}
+
+	void throwForward()
+	{
+		m_holdingObject.rigidbody.AddForce(eyes.transform.forward * pushForce * 100);
+		//Debug.Log("throw forward");
+		m_holdingObject = null;
+	}
+
+	void lightlyDrop()
+			{
+		//Debug.Log("lightly dropped it");
+		m_holdingObject = null;
+	}
+
+	void whipObject()
+	{
+		Vector3 tempVel = Vector3.zero;
+		tempVel += eyes.transform.right * Input.GetAxis("Mouse X") * whipForce;
+		tempVel += eyes.transform.up * Input.GetAxis("Mouse Y") * whipForce;
+		Debug.Log(tempVel);
+		if( tempVel.magnitude > maxWhipForce)
+			tempVel = tempVel.normalized * maxWhipForce;
+		m_holdingObject.rigidbody.AddForce (tempVel, ForceMode.VelocityChange);
+		//Debug.Log("throw with camera");
+		m_holdingObject = null;
+	}
+
+
 }
